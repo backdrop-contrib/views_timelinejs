7609b63585c05ae1e87b06859c418034be412266
Juha Niemi <juha.niemi@mearra.com>
2012-09-26 18:26:49 +0300

    Changed the way TimelineJS is triggered, so it works also with JS/CSS aggregation

--- js/views_timelinejs.js
+++ js/views_timelinejs.js
@@ -1,13 +1,14 @@
 Drupal.behaviors.timelineJS = {
   attach: function(context, settings) {
     // TODO: Make this work with multiple timelines per page
-    if(Drupal.settings.timelineJS['source']) {
-      createStoryJS({
-        type: 'timeline',
-        width: Drupal.settings.timelineJS['width'],
-        height: Drupal.settings.timelineJS['height'],
+    if(VMM.Timeline && Drupal.settings.timelineJS['source']) {
+      var timeline = new VMM.Timeline(
+        Drupal.settings.timelineJS['embed_id'],
+        Drupal.settings.timelineJS['width'],
+        Drupal.settings.timelineJS['height']
+      );
+      timeline.init({
         source: Drupal.settings.timelineJS['source'],
-        embed_id: Drupal.settings.timelineJS['embed_id'],
         lang: Drupal.settings.timelineJS['lang'],
       });
     }
