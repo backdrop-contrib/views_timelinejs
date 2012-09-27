2fe8fc3402e80774eb71e6c551770e6910ed0629
Juha Niemi <juha.niemi@mearra.com>
2012-09-27 10:23:53 +0300

    Issue #1795962 by Pol: Moved PHP processing from .tpl.php to preprocess function.

--- theme/views-timelinejs.tpl.php
+++ theme/views-timelinejs.tpl.php
@@ -1,29 +1,8 @@
 <?php
 /**
- * @file views-timeline.tpl.php
- * View template to output TimelineJS wrapper markup and include scripts
+ * @file views-timelinejs.tpl.php
+ * View template to output TimelineJS wrapper markup
  */
-
-global $language;
-$json_source = '/_timelinejs/' . $view->name . '/' . $view->current_display;
-if(is_array($view->args)) {
-  $json_source .= '/' . implode('/', $view->args);
-}
-$settings = array(
-  'width' => '100%',
-  'height' => '500',
-  'source' => $json_source,
-  'embed_id' => $timelinejs_id,
-  'lang' => $language->language,
-);
-
-// Include TimelineJS libraries
-views_timelinejs_load_libraries();
-
-// Include inline javascript
-drupal_add_js(array('timelineJS' => $settings), 'setting');
-drupal_add_js(drupal_get_path('module', 'views_timelinejs') . '/js/views_timelinejs.js');
-
 ?>
 <div id="<?php print $timelinejs_id ?>" class="timelinejs">
 </div>
