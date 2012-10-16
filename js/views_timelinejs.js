83a30ca0b2920ce5f3c07c730ba7385b3b68350f
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-10-16 13:16:37 +0200

    Fix to get it working when aggregate of JS and CSS is on. Use of the embed_path global variable.

--- js/views_timelinejs.js
+++ js/views_timelinejs.js
@@ -1,7 +1,9 @@
+var embed_path;
 (function ($) {
 
   Drupal.behaviors.timelineJS = {
     attach: function(context, settings) {
+      embed_path = Drupal.settings.timelineJS['embed_path'];
       createStoryJS(Drupal.settings.timelineJS);
     }
   }
