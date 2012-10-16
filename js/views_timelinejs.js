4678f1c2cf4ab8adb292a1eadaa3d7a8bd325b10
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-10-16 22:36:22 +0200

    Better JS detection. Allows you to work with multiple timelineJS in a page. Suppress error when no timelineJS is in the page.

--- js/views_timelinejs.js
+++ js/views_timelinejs.js
@@ -1,10 +1,19 @@
+var embed_path;
+
 (function ($) {
 
   Drupal.behaviors.timelineJS = {
     attach: function(context, settings) {
-      createStoryJS(Drupal.settings.timelineJS);
+      $.each(Drupal.settings.timelineJS, function(key, timeline) {
+        embed_path = timeline['embed_path'];
+        if (timeline['processed'] != true) {
+          createStoryJS(timeline);
+        }
+        timeline['processed'] = true;
+      });
     }
   }
 
 })(jQuery);
+
 
