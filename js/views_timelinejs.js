0a6e09d8901a08cbb25ef8f1025d91977032ab09
Olli Erinko <olli.erinko@mearra.com>
2012-11-08 13:20:08 +0200

    Preliminary work for multiple timelines per page. Re-enabled  preview on view config.

--- js/views_timelinejs.js
+++ js/views_timelinejs.js
@@ -1,17 +1,42 @@
-Drupal.behaviors.timelineJS = {
-  attach: function(context, settings) {
-    // TODO: Make this work with multiple timelines per page
-    if(VMM.Timeline && Drupal.settings.timelineJS['source']) {
-      var timeline = new VMM.Timeline(
-        Drupal.settings.timelineJS['embed_id'],
-        Drupal.settings.timelineJS['width'],
-        Drupal.settings.timelineJS['height']
-      );
-      timeline.init({
-        source: Drupal.settings.timelineJS['source'],
-        lang: Drupal.settings.timelineJS['lang'],
+var embed_path;
+
+(function ($) {
+  Drupal.behaviors.timelineJS = {
+    attach: function(context, settings) {
+      $.each(Drupal.settings.timelineJS, function(key, timeline) {
+        embed_path = timeline['embed_path'];
+
+        if (timeline['processed'] != true) {
+          createStoryJS(timeline);
+        }
+        timeline['processed'] = true;
       });
     }
   }
-}
+})(jQuery);
 
+/*
+(function($) {
+  Drupal.behaviors.timelineJS = {
+    attach: function(context, settings) {
+      console.log(Drupal.settings);
+      // TODO: Make this work with multiple timelines per page
+      $.each(Drupal.settings.timelineJS, function(key, timeline) {
+        console.log(key);
+        console.log(timeline);
+        if(VMM.Timeline && timeline['source']) {
+          var timelineJS = new VMM.Timeline(
+            timeline['embed_id'],
+            timeline['width'],
+            timeline['height']
+          );
+          timelineJS.init({
+            source: timeline['source'],
+            lang: timeline['lang'],
+          });
+        }
+      });
+    }
+  }
+})(jQuery);
+*/
