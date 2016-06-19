83aa216a9d4843220eeb033feb09e016faf5375b
David Cameron <david@cadeyrn.us>
2016-06-19 01:50:57 -0500

    Backported library name and theme function changes from 8.x-3.x.

--- theme/views-timelinejs.tpl.php
+++ theme/views-timelinejs.tpl.php
@@ -4,4 +4,4 @@
  * Views template to output TimelineJS wrapper markup.
  */
 ?>
-<div id="<?php print $timelinejs_id ?>" class="timelinejs" style="width: <?php print $width; ?>; height: <?php print $height; ?>;"></div>
+<div<?php print $div_attributes; ?>></div>
